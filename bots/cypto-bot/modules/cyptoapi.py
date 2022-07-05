2d5d7d9f2d59024c9b579651bc335485e0cfac04
Vinay Chitepu <vinaychitepu@gmail.com>
2022-07-04 20:09:49 -0400

    32: bugfix synax errors

--- bots/cypto-bot/modules/cyptoapi.py
+++ bots/cypto-bot/modules/cyptoapi.py
@@ -9,15 +9,15 @@ def fetch_rates():
     df.rateUsd = pd.to_numeric(df.rateUsd)
     return df
 
-def get_price(df=df, symbol='BTC'):
+def get_price(df, symbol='BTC'):
     pr  = float(df[df.symbol == symbol].rateUsd.values[0])
     return pr
 
-def get_info(df=df, symbol='BTC'):
+def get_info(df, symbol='BTC'):
     info = df[df.symbol == symbol]
     return info
 
-def get_id_from_symbol(df=df, symbol='BTC'):
+def get_id_from_symbol(df, symbol='BTC'):
     i_d = df[df.symbol == symbol].id.values[0]
     return i_d
 
